3733f8fe330d70b5bda44ca86779732cd53b5131
kazuwsx <kazu.wsx@gmail.com>
2021-05-27 00:37:49 +0900

    複数の入力項目を持つフォーム

---
+++ ch3/form_multi/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MultiForm from './MultiForm';
+
+const st = {
+  textAlign: 'left',
+  padding: '10px'
+}
+
+ReactDOM.render(
+  <div style={st}>
+  //   <MultiForm />
+  </div>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
