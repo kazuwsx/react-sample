b85a08573f63cab8927c75ce84c2456ce6a43f7a
kazuwsx <kazu.wsx@gmail.com>
2021-06-06 13:10:03 +0900

    URLのパターンマッチを利用

---
+++ ch5/express_test/dice-ex.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const portNo = 3000
+
+// ルートへのアクセス
+app.get('/', (req, res) => {
+  res.send(
+    '<p><a href="/dice/6">6面体のサイコロ</a><br />' +
+    '<a href="/dice/12">12面体のサイコロ</a></p>')
+})
+// サイコロへのアクセス
+app.get('/dice/:num', (req, res) => {
+  res.send('今回の値は...' + dice(req.params.num))
+})
+function dice(n) {
+  return Math.floor(Math.random() * n) + 1
+}
+
+// 待ち受ける
+app.listen(portNo, () => {
+  console.log('起動しました', `http://localhost:${portNo}`)
+})
