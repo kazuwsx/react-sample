d3be742057af6979129778dfe78beeee7e060286
kazuwsx <kazu.wsx@gmail.com>
2021-06-12 13:15:54 +0900

    React Routerで挨拶を表示するアプリ

---
+++ ch5/router_test/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloApp from './HelloApp';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+ReactDOM.render(
+  <HelloApp />
+  ,document.getElementById('root')
+);
+reportWebVitals();
